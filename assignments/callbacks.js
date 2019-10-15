5dc478ea71e37f1f7a031d17d0e40c2ef08f3764
robby-o <robby.olson11@gmail.com>
2019-10-15 14:47:33 -0700

    fixed callback #5

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -62,12 +62,10 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  if (item in list) {
-    return cb(true);
-  } else {
-    return cb(false);
-  }
+  return cb(list.includes(item))
 }
+
+
 
 /* STRETCH PROBLEM */
 
